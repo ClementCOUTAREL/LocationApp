496af15c6b89ae8d351e6df0e7ddfe8c7f55783f
Clement COUTAREL <coutarel.clement.pro@gmail.com>
2024-03-08 20:41:21 -0500

    Finished location implementation

--- app/src/main/java/com/clementcoutarel/locationapp/MainActivity.kt
+++ app/src/main/java/com/clementcoutarel/locationapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.clementcoutarel.locationapp.ui.theme.LocationAppTheme
@@ -54,6 +56,9 @@ fun LocationDisplay(
     viewModel: LocationViewModel,
     context: Context){
     val location = viewModel.location.value
+    val address = location?.let {
+        locationUtils.reverseGeocodeLocation(location)
+    }
 
     val requestPermissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
@@ -84,9 +89,12 @@ fun LocationDisplay(
         }
     )
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         if(location != null){
-            Text(text = "Address : ${location.latitude} ${location.longitude}")
+            Text(text = "Address : ${location.latitude} ${location.longitude} \n $address" )
         } else {
             Text(text = "Location not available")
         }
